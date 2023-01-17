b8b9dc1c6decccc3db4098ce4954800748db9d9b
EitchKeii <espenkjenstad@gmail.com>
2023-01-17 12:43:13 +0100

    whatever

--- build.bat
+++ build.bat
@@ -1,5 +1,3 @@
 @ECHO OFF
-@MKDIR bin 2>nul
-@MKDIR obj 2>nul
-cl /nologo souls_death_counter.c /Febin\souls_death_counter.exe /Foobj\ /link
+cl /nologo souls_death_counter.c
 IF %ERRORLEVEL% NEQ 0 (echo Error:%ERRORLEVEL% && exit)
